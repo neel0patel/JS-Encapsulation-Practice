07101cb6d3aa23c4c849043e46987900da9bdb69
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-07-12 21:28:25 -0400

    EP

--- public/index.js
+++ public/index.js
@@ -1,41 +1,43 @@
-
 async function main() {
-    // Fetches an array of ingredients, and an array of sandwiches in the users
-    //  cart, at the same time
-    let [fetchedIngredients, fetchedCartItems] = await getAll(`http://localhost:3001/ingredients`, `http://localhost:3001/cart`)
+  // Fetches an array of ingredients, and an array of sandwiches in the users
+  //  cart, at the same time
+  let [fetchedIngredients, fetchedCartItems] = await getAll(
+    `http://localhost:3001/ingredients`,
+    `http://localhost:3001/cart`
+  );
 
-    // Save what we fetched to global variables;
-    ingredients = fetchedIngredients
-    items = fetchedCartItems
+  // Save what we fetched to global variables;
+  ingredientsList.ingredients = fetchedIngredients;
+  cart.items = fetchedCartItems;
 
-    // Select the first sandwich on page load
-    selectSandwich(items[0])
+  // Select the first sandwich on page load
+  cart.selectSandwich(items[0]);
 
-    // Display the ingredients we fetched
-    renderIngredientList();
+  // Display the ingredients we fetched
+  ingredientList.render();
 
-    // Display the sandwiches we fetched
-    renderCart();
+  // Display the sandwiches we fetched
+  cart.render();
 
-    // Attach event listener to the 'Add Sandwich' button
-    let addButton = document.querySelector('.add-button');
-    addButton.addEventListener('click', () => {
-        addSandwich()
-    })
+  // Attach event listener to the 'Add Sandwich' button
+  let addButton = document.querySelector(".add-button");
+  addButton.addEventListener("click", () => {
+    cart.addSandwich();
+  });
 
-    // Attach event listener to the 'Name' input
-    let nameInput = document.querySelector('.name-input');
-    nameInput.addEventListener('input', (e) => {
-        changeSelectedSandwichName(e.target.value)
-    })
+  // Attach event listener to the 'Name' input
+  let nameInput = document.querySelector(".name-input");
+  nameInput.addEventListener("input", (e) => {
+    cart.changeSelectedSandwichName(e.target.value);
+  });
 
-    // Attach event listeners to the radio inputs for selecting a type of bread
-    let breadRadios = document.querySelectorAll('.bread-radio')
-    breadRadios.forEach(radioInput => {
-        radioInput.addEventListener('click', () => {
-            changeSelectedSandwichBread(radioInput.value)
-        })
-    })
+  // Attach event listeners to the radio inputs for selecting a type of bread
+  let breadRadios = document.querySelectorAll(".bread-radio");
+  breadRadios.forEach((radioInput) => {
+    radioInput.addEventListener("click", () => {
+      cart.changeSelectedSandwichBread(radioInput.value);
+    });
+  });
 }
 
-main()
+main();
--- public/ingredientList.js
+++ public/ingredientList.js
@@ -1,63 +1,78 @@
-// The array of ingredients the user can add to a sandwich
-//  This will be updated after we fetch.
-let ingredients = [];
+const ingredientList = {
+  // The array of ingredients the user can add to a sandwich
+  //  This will be updated after we fetch.
+  ingredients: [],
 
-// Updates the DOM to display a list of ingredients
-function renderIngredientList() {
-    const ingredientUl = document.querySelector('.ingredient-list');
+  // Updates the DOM to display a list of ingredients
+  render: function () {
+    const ingredientUl = document.querySelector(".ingredient-list");
 
-    // Empty the ingredientList before adding any content to it.
-    ingredientUl.innerHTML = '';
+    // Empty the ingredientUl before adding any content to it.
+    ingredientUl.innerHTML = "";
 
-    ingredients.forEach(ingredient => {
-        const ingredientDiv = renderIngredientCard(ingredient);
-        ingredientUl.append(ingredientDiv)
-    })
-}
+    this.ingredients.forEach((ingredient) => {
+      const ingredientDiv = this.createIngredientCard(ingredient);
+      ingredientUl.append(ingredientDiv);
+    });
+  },
 
-// Creates a DIV to display a single ingredient
-function renderIngredientCard(ingredient) {
-    const ingredientCard = document.createElement('div');
-    ingredientCard.className = 'card'
+  // Creates a DIV to display a single ingredient
+  createIngredientCard: function (ingredient) {
+    const ingredientCard = document.createElement("div");
+    ingredientCard.className = "card";
 
-    const sandwichHasIngredient = selectedSandwich.ingredients.includes(ingredient.name);
+    const sandwichHasIngredient = cart.selectedSandwich.ingredients.includes(
+      ingredient.name
+    );
     ingredientCard.innerHTML = `
         <div class="card-body">
             <div class="row g-0">
                 <div class="col-sm-4">
-                    <img src="${ingredient.imageURL}" alt="${ingredient.name}" style="width: 100%">
+                    <img src="${ingredient.imageURL}" alt="${
+      ingredient.name
+    }" style="width: 100%">
                 </div>
                 <div class="col-sm-8">
                     <h5 class="card-title">${ingredient.name}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${ingredient.calories} Calories</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">${
+                      ingredient.calories
+                    } Calories</h6>
                     <p class="card-text">${ingredient.description}</p>
-                    ${sandwichHasIngredient ? `
+                    ${
+                      sandwichHasIngredient
+                        ? `
                         <button class="btn btn-danger toggle-button">Remove</button>
-                    ` : `
+                    `
+                        : `
                         <button class="btn btn-primary toggle-button">Add</button>
-                    `}
+                    `
+                    }
                 </div>
             </div>
         </div>
-    `
-    const toggleButton = ingredientCard.querySelector('.toggle-button')
-    toggleButton.addEventListener('click', () => {
-        toggleIngredient(ingredient)
-    })
+    `;
+    const toggleButton = ingredientCard.querySelector(".toggle-button");
+    toggleButton.addEventListener("click", () => {
+      ingredientList.toggleIngredient(ingredient);
+    });
 
-    return ingredientCard
-}
+    return ingredientCard;
+  },
 
-// Runs when the user clicks 'Add' or 'Remove' on a ingredient card
-function toggleIngredient(ingredient) {
-    let sandwichHasIngredient = selectedSandwich.ingredients.includes(ingredient.name);
+  // Runs when the user clicks 'Add' or 'Remove' on a ingredient card
+  toggleIngredient: function (ingredient) {
+    let sandwichHasIngredient = cart.selectedSandwich.ingredients.includes(
+      ingredient.name
+    );
     if (sandwichHasIngredient) {
-        selectedSandwich.ingredients = selectedSandwich.ingredients.filter(x => x !== ingredient.name)
+      cart.selectedSandwich.ingredients =
+        cart.selectedSandwich.ingredients.filter((x) => x !== ingredient.name);
     } else {
-        sandwichHasIngredient = true;
-        selectedSandwich.ingredients.push(ingredient.name)
+      sandwichHasIngredient = true;
+      cart.selectedSandwich.ingredients.push(ingredient.name);
     }
-    saveSelectedSandwich()
-    renderCart()
-    renderIngredientList()
-}
+    cart.saveSelectedSandwich();
+    cart.render();
+    ingredientList.render();
+  },
+};
